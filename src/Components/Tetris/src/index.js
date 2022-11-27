6b794b19d1248baeb6780edbb391b16119475e7e
Ashley Beal <ashleybeal86@yahoo.com>
2022-11-27 13:42:10 -0500

    adding folders and files for Tetris game

---
+++ src/Components/Tetris/src/index.js
@@ -0,0 +1,19 @@
+///  Source Code/Resource: https://tiny-tetris.stackblitz.io  //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Board from "./tetris/board";
+
+function Game() {
+    return (
+        <div className="t-parent">
+            <Board/>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
